3572908361585cced4c739748d46f893b8b28966
harshit <hverma1@cs.iitr.ac.in>
2019-02-13 21:21:51 +0530

    undo btn added

--- logic.js
+++ logic.js
@@ -1,9 +1,25 @@
 var td = document.querySelectorAll("td");
 
-
+document.body.style.zoom = "90%";
 
 for (var i = 0; i < 54; i++)
   td[i].textContent = '';
+
+
+  function Create2DArray(rows) {
+    var arr = [];
+
+    for (var i=0;i<rows;i++) {
+       arr[i] = [];
+    }
+
+    return arr;
+  }
+
+  var arr = Create2DArray(9);
+  for(var i=0;i<9;i++)
+  for(var j=0;j<6;j++)
+  arr[i][j] = 0;
 
 var chance = 0;
 var x = 0;
@@ -12,6 +28,9 @@ var y = 0;
 var dbx = document.getElementById("dialogbox");
 dbx.style.left = window.innerWidth / 2 - 300 + "px";
 dbx.style.top = "400px";
+
+var table = document.querySelector("table");
+
 
 var wrapperdb = document.getElementById("wrapper");
 wrapperdb.style.width = window.innerWidth + "px";
@@ -35,6 +54,16 @@ function ins() {
   window.open('./ins.html', '_blank');
 }
 
+function tdborderred() {
+  for (var i = 0; i < 54; i++)
+    td[i].style.border = "2px solid red";
+}
+
+function tdbordergreen() {
+  for (var i = 0; i < 54; i++)
+    td[i].style.border = "2px solid green";
+}
+
 function redw() {
   red.style.display = "block";
   wrapperdb2.style.display = "block";
@@ -55,7 +84,8 @@ function reset() {
   wrapperdb.style.display = "block";
 }
 
-function yes() {chance =0;
+function yes() {
+  chance = 0;
   for (var i = 0; i < 54; i++)
     td[i].textContent = '';
   dbx.style.display = "none";
@@ -66,15 +96,53 @@ function no() {
   dbx.style.display = "none";
   wrapperdb.style.display = "none";
 }
+var chancer;
+function memoize() {
+  for (var i = 0; i < 9; i++){
+    for (var j = 0; j < 6; j++) {console.log(td[((i) * 6 + j)]);
+      if(td[((i) * 6 + j)].textContent.length == 0)
+      {  arr[i][j] = 0;}
+        else
+      { arr[i][j] = td[((i) * 6 + j)].textContent;}
+    }
+}chancer = chance;
+}
 
+function undo() {
+  if(chance - chancer==1){
+  for (var i = 0; i < 9; i++)
+    for (var j = 0; j < 6; j++) {
+      if(arr[i][j] == 0)
+        td[((i) * 6 + j)].textContent = '';
+        else
+        td[((i) * 6 + j)].textContent = arr[i][j];
+    }
+    if(chance%2==0)
+  {
+    table.style.border = "5px solid green";
+    tdbordergreen();}
+    else {
+
+      table.style.border = "5px solid red";
+      tdborderred();
+    }
+    chance--;}
+
+}
 //logic-starts
 
 function play(x, y) {
   if (td[((x - 1) * 6 + y) - 1].textContent.length == 0 || (chance % 2 == 0 && td[((x - 1) * 6 + y) - 1].style.color === "red") || (chance % 2 != 0 && td[((x - 1) * 6 + y) - 1].style.color === "green")) {
-    if (chance % 2 == 0)
+      memoize();
+    if (chance % 2 == 0) {
       td[((x - 1) * 6 + y) - 1].style.color = "red";
-    else
+      table.style.border = "5px solid green";
+      tdbordergreen();
+    } else {
       td[((x - 1) * 6 + y) - 1].style.color = "green";
+      table.style.border = "5px solid red";
+      tdborderred();
+    }
 
     if (x - 1 == 0) {
 
@@ -200,10 +268,12 @@ function play(x, y) {
 //chain function
 function play2(x, y) {
 
-  if (chance % 2 == 0)
+  if (chance % 2 == 0) {
     td[((x - 1) * 6 + y) - 1].style.color = "red";
-  else
+  } else {
     td[((x - 1) * 6 + y) - 1].style.color = "green";
+  }
+
 
   if (x - 1 == 0) {
 
@@ -335,8 +405,10 @@ function checkwin() {
     if (td[i].style.color === "green")
       g++;
   }
-  if (r == 0)
-  {  greenw(); }
-  if (g == 0)
-  {  redw();}
+  if (r == 0) {
+    greenw();
+  }
+  if (g == 0) {
+    redw();
+  }
 }
